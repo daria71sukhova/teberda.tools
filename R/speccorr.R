ad2b7ce5807b99c66fffbc95147a6585cfe4f13b
daria71sukhova <daria.sukhova@gmail.com>
2020-10-08 10:52:55 +0300

    speccorr.R is modified

--- R/speccorr.R
+++ R/speccorr.R
@@ -1,11 +1,12 @@
+#' @title Simple formatting of a correlation matrix
+#' @description This function provides a simple formatting of a correlation matrix
+#' into a table with 4 columns containing :
+#' Column 1 : row names (variable 1 for the correlation test)
+#' Column 2 : column names (variable 2 for the correlation test)
+#' Column 3 : the correlation coefficients
+#' Column 4 : the p-values of the correlations
 
 flat_cor_mat <- function(cor_r, cor_p){
-  #This function provides a simple formatting of a correlation matrix
-  #into a table with 4 columns containing :
-  # Column 1 : row names (variable 1 for the correlation test)
-  # Column 2 : column names (variable 2 for the correlation test)
-  # Column 3 : the correlation coefficients
-  # Column 4 : the p-values of the correlations
   cor_r <- tibble::rownames_to_column(as.data.frame(cor_r), var = "row")
   cor_r <- tidyr::gather(cor_r, column, cor, -1)
   cor_p <- tibble::rownames_to_column(as.data.frame(cor_p), var = "row")
@@ -36,11 +37,12 @@ flat_cor_mat <- function(cor_r, cor_p){
 #' @export
 
 speccorr <- function(data_file,
+                     data_fie2 = NULL,
                      need_abbr = FALSE,
                      state = NULL,
                      type = "pearson",
                      pVal = 0.05){
-  df_data <- get_tidy_data(data_file, need_abbr, state)
+  df_data <- get_tidy_data(data_file, data_fie2, need_abbr, state)
   df_data <- df_data[, -1]
   if(type != "pearson"){
     corr_matrix <- Hmisc::rcorr(as.matrix(df_data), type = "spearman")
